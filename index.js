159d83abdd06da739757e2143d32166270a836da
Emilio Cuevas Rend <emicueren@alum.us.es>
2026-02-28 13:25:48 +0100

    Configurado puerto dinámico para despliegue en Render

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ app.get('/samples/MRR', (req, res) => {
 
 
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log('Server is running on http://localhost:' + PORT);
 });
