4ddb3ea4ababce0c738934c7026f9c4bd44d1791
mariarguezro <mariarguezro@gmail.com>
2026-03-02 12:29:22 +0100

    index mrr hecho

--- index.js
+++ index.js
@@ -71,6 +71,7 @@ app.get('/cool', (req, res) => {
 // -----------------------------------------------------------------
 // MARÍA RODRÍGUEZ ROMERO (MRR)
 // -----------------------------------------------------------------
+
 app.get('/samples/MRR', (req, res) => {
     let regionElegida = ["North America", "Asia", "Europe"];
     let texto = "";
@@ -90,7 +91,7 @@ app.get(BASE_URL_API + "/online-sales-popular-marketplaces", (req, res) => {
 
 app.get(BASE_URL_API + "/online-sales-popular-marketplaces/loadInitialData", (req, res) => {
   if (datosMRR.length !== 0) {
-    res.status(409, "CONFLICT").json({message: "Ya existen datos"});
+    return res.status(409, "CONFLICT").json({message: "Ya existen datos"});
   }
 
   let newData = [{region: "North America", date: "2024-01-01", product_category: "Electronics", product_name: "iPhone 14 Pro", quantity_sold: 2, unit_price: 999.99, total: 1999.98, payment_method: "Credit Card"},
@@ -104,20 +105,72 @@ app.get(BASE_URL_API + "/online-sales-popular-marketplaces/loadInitialData", (re
     {region: "Asia", date: "2024-07-07", product_category: "Clothing", product_name: "Nike Dri-FIT Training Shorts", quantity_sold: 4, unit_price: 34.99, total: 139.96, payment_method: "Debit Card"},
     {region: "Europe", date: "2024-08-20", product_category: "Beauty Products", product_name: "Fresh Sugar Lip Treatment", quantity_sold: 1, unit_price: 24, total: 24, payment_method: "PayPal"},
     {region: "Europe", date: "2024-04-16", product_category: "Beauty Products", product_name: "The Ordinary Niacinamide Serum", quantity_sold: 1, unit_price: 6.5, total: 6.5, payment_method: "PayPal"}
-    ];
+  ];
 
-    datosMRR.push(newData);
-  res.status(200, "OK").json(datosMRR);
+  datosMRR.push(newData);
+  return res.status(200, "OK").json(datosMRR);
 });
 
 app.post(BASE_URL_API + "/online-sales-popular-marketplaces", (req, res) => {
-    
+  let newSale = req.body;
+  if (!newSale.region || !newSale.date || !newSale.product_category || !newSale.product_name || !newSale.quantity_sold
+    || !newSale.unit_price || !newSale.total || !newSale.payment_method){
+        return res.status(400, "BAD REQUEST").json({message: "Es posible que falte algún elemento"});
+  };
+  let existe = datosMRR.some(sale => sale.region === newSale.region || sale.date === newSale.date || sale.product_name === newSale.product_name);
+  if (existe){
+    return res.status(409, "CONFLICT").json({message: "Existe un dato idéntico al que se quiere añadir"})
+  }
+  datosMRR.push(newSale);
+  return res.status(201, "CREATED").json({message: "Dato nuevo creado"});
+});
+
+app.put(BASE_URL_API + "/online-sales-popular-marketplaces", (req, res) => {
+  res.status(405, "METHOD NOT ALLOWED").json({message: "No es posible actualizar la lista de datos"})
+});
+
+app.delete(BASE_URL_API + "/online-sales-popular-marketplaces", (req, res) => {
+  datosMRR = [];
+  res.status(200, "OK").json({message: "Datos eliminados"})
 });
 
 app.get(BASE_URL_API + "/onlines-sales-popular-marketplaces/:region", (req, res) => {
     let regionName = req.params.region;
-
+    let filtro = datosMRR.filter(sale => sale.region === regionName);
+    res.status(200, "OK").json(filtro);
 });
+
+app.post(BASE_URL_API + "/onlines-sales-popular-marketplaces/:region", (req, res) => {
+    res.status(405, "METHOD NOT ALLOWED").json({message: "No es posible añadir valores a un dato o varios datos"})
+});
+
+app.put(BASE_URL_API + "/onlines-sales-popular-marketplaces/:region", (req, res) => {
+    let regionName = req.params.region;
+    let dateN = req.params.date;
+    let id = datosMRR.findIndex(d => d.region === regionName && d.date === dateN);
+
+    if (id === -1) {
+        return res.status(404, "NOT FOUND").json({ message: "Recurso no encontrado" });
+    }
+
+    datosMRR[id] = req.body;
+    res.status(200, "OK").json(datos[id]);
+});
+
+
+app.delete(BASE_URL_API + "/onlines-sales-popular-marketplaces/:region", (req, res) => {
+    let regionName = req.params.region;
+    let dateN = req.params.date;
+    let index = datosMRR.findIndex(d => d.region === regionName && d.date === dateN);
+
+    if (index === -1) {
+        return res.status(404).json({ message: "No encontrado" });
+    }
+
+    datosMRR.splice(index, 1);
+    res.status(200, "OK").json(datosMRR);
+});
+
 
 // -----------------------------------------------------------------
 // RUTA DE DAV
