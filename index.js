409e41fa8c35dd6ee9fe1a99ff352cf302e8bbe1
David Ayllón <davidayllon5@gmail.com>
2026-03-06 14:20:59 +0100

    arreglo de filtrado

--- index.js
+++ index.js
@@ -217,6 +217,9 @@ app.get(BASE_URL_API + "/google-ads-performance", (req, res) => {
     //filtro plataforma
     if (platform) filteredData = filteredData.filter(d => d.platform.toLowerCase() === platform.toLowerCase());
     
+    if (industry) {
+        filteredData = filteredData.filter(d => d.industry.toLowerCase() === industry.toLowerCase());
+    }
     // Filtrado por rango de fechas (ISO strings funcionan bien para comparación alfabética)
     if (from && to) {
         filteredData = filteredData.filter(d => d.date >= from && d.date <= to);
