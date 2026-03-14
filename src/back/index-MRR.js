6ae29074a37f108d27061ac9edde335d4e9bf5e6
mariarguezro <mariarguezro@gmail.com>
2026-03-14 12:53:54 +0100

    cambio de orden mrr

--- src/back/index-MRR.js
+++ src/back/index-MRR.js
@@ -82,6 +82,11 @@ export function loadBackEndMRR(app){
     res.status(200, "OK").json({message: "Datos eliminados"})
     });
 
+    app.get(BASE_URL_API + "/online-sales-popular-marketplaces/docs", (req, res) =>{
+        res.redirect('https://documenter.getpostman.com/view/52406430/2sBXigLYQR');
+    });
+
+
     app.get(BASE_URL_API + "/online-sales-popular-marketplaces/:region/:date", (req, res) => {
         let filtrado = datosMRR;
         let regionName = req.params.region;
@@ -148,10 +153,7 @@ export function loadBackEndMRR(app){
         res.status(200, "OK").json(datosMRR);
     });
 
-    app.get(BASE_URL_API + "/online-sales-popular-marketplaces/docs", (req, res) =>{
-        res.redirect('https://documenter.getpostman.com/view/52406430/2sBXigLYQR');
-    });
-
+    
 }
 
 
