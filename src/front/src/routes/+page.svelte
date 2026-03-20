4d015c6a62ff6905e547eab040efeabaefe15929
mariarguezro <mariarguezro@gmail.com>
2026-03-20 18:48:38 +0100

    .

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -1,5 +1,39 @@
+<style>
+    div {
+        align-content: center;
+    }
+</style>
 
+<h1> SOS2526-23 </h1>
 
+<h2> Team </h2>
+<ul>
+    <li> David Ayllón Vela - global-ads-performance </li>
+    <li> Emilio Cuevas Rendón - daily-global-stock-market-indicators</li>
+    <li> María Rodríguez Romero - online-sales-popular-marketplaces</li>
+</ul>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2> Repositorio de Github </h2>
+<p> &rarr; <a href="https://github.com/gti-sos/SOS2526-23" target="_blank"> https://github.com/gti-sos/SOS2526-23 </a></p>
+
+<h2> Front-end individual </h2>
+<div>
+    <button> <a href="" target="_blank"> DAV </a></button>
+    <button> <a href="" target="_blank"> ECR </a></button>
+    <button> <a href="" target="_blank"> MRR </a></button>
+</div>
+
+<h2> API's desarrolladas </h2>
+<div>
+    <button> <a href="" target="_blank"> global-ads-performance </a></button>
+    <button> <a href="" target="_blank"> daily-global-stock-market-indicators </a></button>
+    <button> <a href="" target="_blank"> online-sales-popular-marketplaces </a></button>
+</div>
+
+<h2> Documentación de Postman </h2>
+<div>
+    <button> <a href="" target="_blank"> global-ads-performance </a></button>
+    <button> <a href="" target="_blank"> daily-global-stock-market-indicators </a></button>
+    <button> <a href="" target="_blank"> online-sales-popular-marketplaces </a></button>
+</div>
+
