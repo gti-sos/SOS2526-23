251b48bda6570220c903a04051e32d4df355bc74
Emilio Cuevas Rend <emicueren@alum.us.es>
2026-03-21 14:29:32 +0100

    Añade vista principal completada con enlaces

--- src/front/src/routes/+page.svelte
+++ src/front/src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <style>
     div {
         align-content: center;
+        margin-bottom: 20px;
+    }
+    button {
+        margin-right: 10px;
+        cursor: pointer;
+        padding: 5px 10px;
     }
 </style>
 
@@ -18,22 +24,21 @@
 
 <h2> Front-end individual </h2>
 <div>
-    <button> <a href="" target="_blank"> DAV </a></button>
-    <button> <a href="" target="_blank"> ECR </a></button>
-    <button> <a href="" target="_blank"> MRR </a></button>
+    <a href="/global-ads-performance"><button> DAV </button></a>
+    <a href="/daily-global-stock-market-indicators"><button> ECR </button></a>
+    <a href="/online-sales-popular-marketplaces"><button> MRR </button></a>
 </div>
 
 <h2> API's desarrolladas </h2>
 <div>
-    <button> <a href="" target="_blank"> global-ads-performance </a></button>
-    <button> <a href="" target="_blank"> daily-global-stock-market-indicators </a></button>
-    <button> <a href="" target="_blank"> online-sales-popular-marketplaces </a></button>
+    <a href="/api/v1/global-ads-performance" target="_blank"><button> global-ads-performance </button></a>
+    <a href="/api/v1/daily-global-stock-market-indicators" target="_blank"><button> daily-global-stock-market-indicators </button></a>
+    <a href="/api/v1/online-sales-popular-marketplaces" target="_blank"><button> online-sales-popular-marketplaces </button></a>
 </div>
 
 <h2> Documentación de Postman </h2>
 <div>
-    <button> <a href="" target="_blank"> global-ads-performance </a></button>
-    <button> <a href="" target="_blank"> daily-global-stock-market-indicators </a></button>
-    <button> <a href="" target="_blank"> online-sales-popular-marketplaces </a></button>
+    <a href="https://sos2526-23.onrender.com/api/v1/global-ads-performance/docs" target="_blank"><button> global-ads-performance </button></a>
+    <a href="https://sos2526-23.onrender.com/api/v1/daily-global-stock-market-indicators/docs" target="_blank"><button> daily-global-stock-market-indicators </button></a>
+    <a href="https://sos2526-23.onrender.com/api/v1/online-sales-popular-marketplaces/docs" target="_blank"><button> online-sales-popular-marketplaces </button></a>
 </div>
-
