23a08d89cf791378a8dd9a4a186dd0e8a80868cb
David Ayllón <davidayllon5@gmail.com>
2026-04-11 12:21:14 +0200

    Auth0

--- src/front/src/lib/auth.js
+++ src/front/src/lib/auth.js
@@ -6,9 +6,9 @@ export async function initAuth() {
     auth0Client = await createAuth0Client({
         // CORRECCIÓN: Quitamos el "https://" y la "/" final
         domain: 'dev-dxwqup0hqj5q6tuz.eu.auth0.com', 
-        clientId: '8PwsroYENDJ18pQnpcW98eW10w8kd9tO',       
+        clientId: 'XX4N1x0i9z10kmc5wv7dyfDcfpX4RMHZ',       
         authorizationParams: {
-            redirect_uri: window.location.origin,
+            redirect_uri: window.location.origin + '/global-ads-performance',
             audience: 'https://api.sos2526-23.com'
         }
     });
@@ -22,7 +22,7 @@ export async function login() {
 
 // Función para manejar el logout
 export async function logout() {
-    await auth0Client.logout({ logoutParams: { returnTo: window.location.origin } });
+    await auth0Client.logout({ logoutParams: { returnTo: window.location.origin + '/global-ads-performance' } });
 }
 
 // Función para obtener el token de acceso
