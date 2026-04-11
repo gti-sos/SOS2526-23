aecbeb5d0f3297d9b337bd129b200de64faa119a
Emilio Cuevas Rend <emicueren@alum.us.es>
2026-04-11 16:51:49 +0200

    Configuración híbrida para Vercel y Render

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapterAuto from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// Detectamos si estamos en Vercel usando una variable de entorno que ellos inyectan siempre
+const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
-	vitePlugin: {
-		dynamicCompileOptions: ({ filename }) =>
-			filename.includes('node_modules') ? undefined : { runes: true }
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        // Si es Vercel usa 'auto', si es Render (o cualquier otro) usa 'node'
+        adapter: isVercel ? adapterAuto() : adapterNode()
+    },
+    vitePlugin: {
+        dynamicCompileOptions: ({ filename }) =>
+            filename.includes('node_modules') ? undefined : { runes: true }
+    }
 };
 
 export default config;
