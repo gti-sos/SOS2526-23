7b4166eca4768a254c621124c28a22bd2ea81a34
Emilio Cuevas Rend <emicueren@alum.us.es>
2026-04-12 20:24:04 +0200

    Corregir error svelte.config

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -1,11 +1,16 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapterVercel from '@sveltejs/adapter-vercel';
+import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// Esta variable detecta si estamos en el entorno de Vercel
+const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        adapter: adapter()
+        // DINÁMICO: Si es Vercel, usa su adaptador. Si es GitHub Actions, usa Node.
+        adapter: isVercel ? adapterVercel() : adapterNode()
     },
     vitePlugin: {
         dynamicCompileOptions: ({ filename }) =>
