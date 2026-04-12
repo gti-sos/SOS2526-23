4d966d1dcb57d36efa98145527e581d7199ca328
Emilio Cuevas Rend <emicueren@alum.us.es>
2026-04-12 20:12:28 +0200

    Fix: Resolviendo conflictos en la config

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -2,20 +2,12 @@ import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-<<<<<<< Updated upstream
-=======
-// Vercel inyecta automáticamente esta variable de entorno
->>>>>>> Stashed changes
 const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-<<<<<<< Updated upstream
-=======
-        // Usa Vercel en la nube y Node en GitHub Actions/Local
->>>>>>> Stashed changes
         adapter: isVercel ? adapterVercel() : adapterNode()
     },
     vitePlugin: {
