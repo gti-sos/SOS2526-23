40dced993292672790883b367097ee9588283b2f
Emilio Cuevas Rend <emicueren@alum.us.es>
2026-04-12 20:40:47 +0200

    Fix: corregida sintaxis duplicada en svelte.config

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -2,14 +2,12 @@ import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// Esta variable detecta si estamos en el entorno de Vercel
 const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
-        // DINÁMICO: Si es Vercel, usa su adaptador. Si es GitHub Actions, usa Node.
         adapter: isVercel ? adapterVercel() : adapterNode()
     },
     vitePlugin: {
