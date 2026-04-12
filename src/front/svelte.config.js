3bdbc0b132f3ce46020a3d032b6f17a603f5b3e0
Emilio Cuevas Rend <emicueren@alum.us.es>
2026-04-12 20:10:57 +0200

    Fix: Adaptador dinámico Vercel/Node

--- src/front/svelte.config.js
+++ src/front/svelte.config.js
@@ -2,12 +2,20 @@ import adapterVercel from '@sveltejs/adapter-vercel';
 import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+<<<<<<< Updated upstream
+=======
+// Vercel inyecta automáticamente esta variable de entorno
+>>>>>>> Stashed changes
 const isVercel = process.env.VERCEL === '1';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: vitePreprocess(),
     kit: {
+<<<<<<< Updated upstream
+=======
+        // Usa Vercel en la nube y Node en GitHub Actions/Local
+>>>>>>> Stashed changes
         adapter: isVercel ? adapterVercel() : adapterNode()
     },
     vitePlugin: {
